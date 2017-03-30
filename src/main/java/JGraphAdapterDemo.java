f0784e5984b2a62f7a9bd68adfa839f7ef2d0976
Combii <davidstovle@gmail.com>
2017-03-30 16:38:40 +0200

    Stuff

--- src/main/java/JGraphAdapterDemo.java
+++ src/main/java/JGraphAdapterDemo.java
@@ -17,17 +17,14 @@
  */
 
 import java.awt.*;
-import java.awt.geom.*;
 
 import javax.swing.*;
 
 import org.jgraph.*;
-import org.jgraph.graph.*;
-import org.jgrapht.*;
 import org.jgrapht.ext.*;
 import org.jgrapht.graph.*;
 // resolve ambiguity
-import org.jgrapht.graph.DefaultEdge;
+
 
 /**
  * A demo applet that shows how to use JGraph to visualize JGraphT graphs.
